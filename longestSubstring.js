bd2010cb7f95fa4fd6fa27325e164417221062b1
josimuddin198 <josimweb0504@gmail.com>
2026-01-27 21:48:59 +0600

    feat: implement longest substring without repeating characters

---
+++ longestSubstring.js
@@ -0,0 +1,20 @@
+const longestSubstring = (str) => {
+  let set = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < str.length; right++) {
+    while (set.has(str[right])) {
+      set.delete(str[left]);
+      left++;
+    }
+
+    set.add(str[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+const result = longestSubstring("pwwkew");
+console.log(result);
